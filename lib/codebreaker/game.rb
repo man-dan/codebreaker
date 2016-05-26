ee3cae96f0d1126fede446353a20a65b0aee5b4b
Den <man-dan@mail.ru>
2016-05-26 13:07:47 +0300

    Main methods of codebreaker

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,11 +1,29 @@
 module Codebreaker
   class Game
+    attr_accessor :secret_code
     def initialize
-      @secret_code = ""
+      @secret_code = (0...4).map { rand(1..6) }.join
+      @turns = 10
+      @hints = 1
+      @game_result = ''
     end
- 
-    def start
-      @secret_code = "1234"
+
+    def check(guess)
+      mark = ''
+      @turns -=1
+      codes = @secret_code.chars.zip(guess.to_s.chars)
+      mark<< '+' * codes.select {|x,y| x==y }.count
+      minus = codes.delete_if {|x,y| x==y }
+      mark<< '-' * (minus.transpose[0] & minus.transpose[1]).count unless minus.empty?
+      mark
     end
+
+    def hint
+       return "0 hints remain" if @hints==0
+       @hints -=1
+       @secret_code.chars.sample
+    end
+
   end
 end
+
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,13 +2,8 @@ require 'spec_helper'
 
 module Codebreaker
   RSpec.describe Game do
-    context "#start" do
-      let(:game) { Game.new }
- 
-      before do
-        game.start
-      end
- 
+    let(:game) { Game.new }
+    context "#init" do
       it "saves secret code" do
         expect(game.instance_variable_get(:@secret_code)).not_to be_empty
       end
@@ -19,6 +14,46 @@ module Codebreaker
  
       it "saves secret code with numbers from 1 to 6" do
         expect(game.instance_variable_get(:@secret_code)).to match(/[1-6]+/)
+      end
+    end
+
+    context "#check guess" do
+      before do
+        game.instance_variable_set(:@secret_code,"1234")
+      end
+
+      it "game_result win" do
+        expect(game.check(1234)).to eq("++++")
+      end
+
+      it "game_result not guessed" do
+        expect(game.check(5656)).to eq("")
+      end
+
+      it "game_result not guessed positions" do
+        expect(game.check(2143)).to eq("----")
+      end
+
+      it "check - reduce turns" do
+        game.check(2143)
+        expect(game.instance_variable_get(:@turns)).to eq(9)
+      end
+    end
+
+    context "#hints" do
+
+      it "reduce hints" do
+        game.hint
+        expect(game.instance_variable_get(:@hints)).to eq(0)
+      end
+      
+      it "hint" do
+        expect(game.instance_variable_get(:@secret_code)).to include(game.hint)
+      end
+
+      it "0 hints" do
+        game.hint
+        expect(game.hint).to eq("0 hints remain")
       end
     end
   end
